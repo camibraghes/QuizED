497c05e6727b603ce6e5774d40f076d71d8317af
Camelia <cameliabraghes@gmail.com>
2023-07-13 16:33:01 +0300

    Setup commit

---
+++ QuizEssentialDeveloper/QuizEssentialDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizEssentialDeveloperApp.swift
+//  QuizEssentialDeveloper
+//
+//  Created by Camelia Braghes on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizEssentialDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizEssentialDeveloperTests/QuizEssentialDeveloperTests.swift
@@ -0,0 +1,13 @@
+//
+//  QuizEssentialDeveloperTests.swift
+//  QuizEssentialDeveloperTests
+//
+//  Created by Camelia Braghes on 13.07.2023.
+//
+
+import XCTest
+@testable import QuizEssentialDeveloper
+
+final class QuizEssentialDeveloperTests: XCTestCase {
+
+}
