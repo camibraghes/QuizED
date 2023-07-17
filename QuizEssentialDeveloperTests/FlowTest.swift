0cfe4f8c90ddaa7daefa5689e2cf7dbd8a75850d
Camelia <cameliabraghes@gmail.com>
2023-07-17 19:15:58 +0300

    Implement Tests with Closures

--- QuizEssentialDeveloperTests/FlowTest.swift
+++ QuizEssentialDeveloperTests/FlowTest.swift
@@ -41,12 +41,34 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
 
+    func testStartTwiceWithTwoQuestionsRoutesToFirstQuestionTwice() {
+        let router = RouterSpy()
+        let sut = Flow(router: router, questions: ["Q1", "Q2"])
+        
+        sut.start()
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q1"])
+    }
+    
+    func testStartAndAnswearFirstQuestionWithTwoQuestionsRoutesToSecondQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(router: router, questions: ["Q1", "Q2"])
+        
+        sut.start()
+        
+        router.answearCallBack("A1")
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
+    }
     
     class RouterSpy: Router {
         var routedQuestions: [String] = []
+        var answearCallBack: ((String) -> Void) = { _ in}
         
-        func routeTo(question: String) {
+        func routeTo(question: String, answearCallBack: @escaping (String) -> Void) {
             routedQuestions.append(question)
+            self.answearCallBack = answearCallBack
         }
     }
 }
